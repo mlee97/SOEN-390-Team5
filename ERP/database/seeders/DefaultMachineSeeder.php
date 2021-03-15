ae03bb0194eb5310855f76905f28547ba0a0ebf3
WanProduction <48234317+WanProduction@users.noreply.github.com>
2021-03-15 01:46:17 -0400

    Update DefaultMachineSeeder.php

--- ERP/database/seeders/DefaultMachineSeeder.php
+++ ERP/database/seeders/DefaultMachineSeeder.php
@@ -15,36 +15,66 @@ class DefaultMachineSeeder extends Seeder
     public function run()
     {
 
-        $seatMachine = new Machine();
-        $seatMachine -> name = 'Seat Machine';
-        $seatMachine -> status = 'offline';
+        $seatMachine1 = new Machine();
+        $seatMachine1 -> name = 'Seat Machine 1';
+        $seatMachine1 -> status = 'offline';
 
-        $frameMachine = new Machine();
-        $frameMachine -> name = 'Frame Machine';
-        $frameMachine -> status = 'offline';
+        $seatMachine2 = new Machine();
+        $seatMachine2 -> name = 'Seat Machine 2';
+        $seatMachine2 -> status = 'offline';
 
-        $wheelMachine = new Machine();
-        $wheelMachine -> name = 'Wheel Machine';
-        $wheelMachine -> status = 'offline';
+        $frameMachine1 = new Machine();
+        $frameMachine1 -> name = 'Frame Machine 1';
+        $frameMachine1 -> status = 'offline';
 
-        $pedalMachine = new Machine();
-        $pedalMachine -> name = 'Pedal Machine';
-        $pedalMachine -> status = 'offline';
+        $frameMachine2 = new Machine();
+        $frameMachine2 -> name = 'Frame Machine 2';
+        $frameMachine2 -> status = 'offline';
 
-        $handleMachine = new Machine();
-        $handleMachine -> name = 'Handle Machine';
-        $handleMachine -> status = 'offline';
+        $wheelMachine1 = new Machine();
+        $wheelMachine1 -> name = 'Wheel Machine 1';
+        $wheelMachine1 -> status = 'offline';
 
-        $assemblerMachine = new Machine();
-        $assemblerMachine -> name = 'Assembler Machine';
-        $assemblerMachine -> status = 'offline';
+        $wheelMachine2 = new Machine();
+        $wheelMachine2 -> name = 'Wheel Machine 2';
+        $wheelMachine2 -> status = 'offline';
 
-        $seatMachine->save();
-        $frameMachine->save();
-        $wheelMachine->save();
-        $pedalMachine->save();
-        $handleMachine->save();
-        $assemblerMachine->save();
+        $pedalMachine1 = new Machine();
+        $pedalMachine1 -> name = 'Pedal Machine 1';
+        $pedalMachine1 -> status = 'offline';
+         
+        $pedalMachine2 = new Machine();
+        $pedalMachine2 -> name = 'Pedal Machine 2';
+        $pedalMachine2 -> status = 'offline';
+
+        $handleMachine1 = new Machine();
+        $handleMachine1 -> name = 'Handle Machine 1';
+        $handleMachine1 -> status = 'offline';
+
+        $handleMachine2 = new Machine();
+        $handleMachine2 -> name = 'Handle Machine 2';
+        $handleMachine2 -> status = 'offline';
+
+        $assemblerMachine1 = new Machine();
+        $assemblerMachine1 -> name = 'Assembler Machine 1';
+        $assemblerMachine1 -> status = 'offline';
+
+        $assemblerMachine2 = new Machine();
+        $assemblerMachine2 -> name = 'Assembler Machine 2';
+        $assemblerMachine2 -> status = 'offline';
+
+        $seatMachine1->save();
+        $seatMachine2->save();
+        $frameMachine1->save();
+        $frameMachine2->save();
+        $wheelMachine1->save();
+        $wheelMachine2->save();
+        $pedalMachine1->save();
+        $pedalMachine2->save();
+        $handleMachine1->save();
+        $handleMachine2->save();
+        $assemblerMachine1->save();
+        $assemblerMachine2->save();
         
     }
 }
