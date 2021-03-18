c0b43fe91586c6044345681220b7f80e4733200b
xujulien99 <xujulien99@gmail.com>
2021-03-17 23:48:10 -0400

    Added PDF export button

--- ERP/app/Http/Controllers/LogController.php
+++ ERP/app/Http/Controllers/LogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Log;
 use Illuminate\Http\Request;
+use PDF;
 use Illuminate\Support\Facades\DB;
 
 class LogController extends Controller
@@ -51,4 +52,51 @@ class LogController extends Controller
 
     }
 
+    //functions to create PDF
+    function get_logs()
+    {
+     $logs = DB::table('logs')
+         ->limit(10)
+         ->get();
+     return $logs;
+    }
+
+    function pdf()
+    {
+     $pdf = \App::make('dompdf.wrapper');
+     $pdf->loadHTML($this->convert_logs_to_html());
+     return $pdf->stream();
+    }
+
+    function convert_logs_to_html()
+    {
+     $logs = $this->get_logs();
+     $output = '
+     <h3 align="center">logs</h3>
+     <table width="100%" style="border-collapse: collapse; border: 0px;">
+      <tr>
+      <th style="border: 1px solid; padding:12px;" width="10%">Type</th>
+      <th style="border: 1px solid; padding:12px;" width="20%">TimeStamp</th>
+      <th style="border: 1px solid; padding:12px;" width="20%">IP Address</th>
+      <th style="border: 1px solid; padding:12px;" width="20%">Message</th>
+      <th style="border: 1px solid; padding:12px;" width="20%">Request Type</th>
+      </tr>
+      ';  
+
+     foreach($logs as $log)
+        {
+        $output .= '
+        <tr>
+        <td style="border: 1px solid; padding:12px;">'.$log->log_type.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$log->created_at.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$log->ip_address.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$log->message.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$log->request_type.'</td>
+        </tr>
+        ';
+        }
+
+     $output .= '</table>';
+     return $output;
+    }
 }
