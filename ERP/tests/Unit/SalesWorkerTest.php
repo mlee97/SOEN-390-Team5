6e7606261b170d0b4ac444ee8dbc1307d71a6b6e
Michael Lee <mleelwh@gmail.com>
2021-04-07 23:33:13 -0400

    feat: added unit tests for Accountants and Sales #206
    Added unit tests for accountants and sales.

--- ERP/tests/Unit/SalesWorkerTest.php
+++ ERP/tests/Unit/SalesWorkerTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\Bike;
+use App\Models\Sale;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use function PHPUnit\Framework\assertEquals;
 
 class SalesWorkerTest extends TestCase
 {
@@ -33,5 +36,33 @@ class SalesWorkerTest extends TestCase
         $response2 = $this->actingAs($user2)->get('/sales');
 
         $response2->assertStatus(302); //Should redirect
+    }
+
+    public function test_save_order_works_for_sales_only()
+    {
+        //Create sales user
+        $user = User::factory()->create();
+        $user->user_type = 8;
+
+        //Create a test bike
+        $test_bike = Bike::create([
+            'type' => 'test',
+            'size' =>'test',
+            'color' => 'test',
+            'price' => 12,
+            'finish' => 'test',
+            'grade' => 'test',
+            'quantity_in_stock' => 1222,
+        ]);
+
+        $this->actingAs($user)->post('/sales', [
+            'bicycleId' => $test_bike->id,
+            'quantitySold' => 2,
+            'profit' => 300
+        ]);
+        
+        $sales = Sale::all();
+        assertEquals(1, $sales->count());
+
     }
 }
