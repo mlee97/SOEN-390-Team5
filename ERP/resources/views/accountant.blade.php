6f503bcd64d2254a822886d24f0aefa33a9b3c92
Pasha Pishdad <pasha.pishdad@gmail.com>
2021-04-07 21:28:39 -0400

    issue 215: removing monthly and yearly tabs from the accountant page

--- ERP/resources/views/accountant.blade.php
+++ ERP/resources/views/accountant.blade.php
@@ -12,12 +12,6 @@
                     <li class="nav-item">
                         <a class="nav-link active" data-toggle="tab" href="#all_sales">All Sales</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#monthly_sales">Monthly Sales</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="tab" href="#yearly_sales">Yearly Sales</a>
-                    </li>
                     <li class="nav-item ml-auto">
                         <span data-href="/sales-CSV-export" id="export" class="btn btn-primary btn-sm" onclick="exportToCSV(event.target);">Export into CSV</span>           
                     </li>
@@ -73,7 +67,6 @@
                             @endforeach
                         </tbody>
                     </table>
-                    
                     <table class="table table-bordered">                   
                         <tbody>
                             <tr>
@@ -94,155 +87,6 @@
                         </tbody>
                     </table>
                 </div>
-                <!--End of all sales tab-->             
-
-                <!--Monthly sales tab-->
-                <div class="tab-pane fade" id="monthly_sales">
-                    <table class="table table-borderless">
-                        <tbody>
-                            <tr>
-                                <td align='left'><h3>This month's sales</h3></td> <!--Make the month dynamic--><!--Inline css (align='left'), for better practice, can be taken out into a css file-->
-                                <td align='right'>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose a month</button>
-                                        <div class="dropdown-menu">
-                                            <!--Dummy values, make them dynamic in the future-->
-                                            <a class="dropdown-item" href="#">{{$currentMonth}}/{{$currentYear}}</a>
-                                            <a class="dropdown-item" href="#">{{$currentMonth-1}}/{{$currentYear}}</a>
-                                            <a class="dropdown-item" href="#">{{$currentMonth-2}}/{{$currentYear}}</a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col" rowspan = "2">Sales ID</th>
-                                <th scope="col" colspan = "7">Bicycle Specifications</th>
-                                <th scope="col" rowspan = "2">Quantity Sold</th>
-                                <th scope="col" rowspan = "2">Date Sold</th>
-                                <th scope="col" rowspan = "2">Profit (CAD)</th>
-                            </tr>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Type</th>
-                                <th scope="col">Size</th>
-                                <th scope="col">Color</th>
-                                <th scope="col">Finish</th>
-                                <th scope="col">Grade</th>
-                                <th scope="col">Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!--Dummy values, make them dynamic in the future-->
-                            @foreach ($sales as $sale)
-                                @foreach ($sale->bikes as $bikeSale)
-                                <tr>
-                                    <td>{{$sale->id}}</td>
-                                    
-                                    <td>{{$bikeSale->bike_sale_pivot->bike_id}}</td> <!--We need to use bike_sale_pivot to get the bike_id in the bike_sale table-->
-                                    <td>{{$bikeSale->type}}</td>
-                                    <td>{{$bikeSale->size}}</td>
-                                    <td>{{$bikeSale->color}}</td>
-                                    <td>{{$bikeSale->finish}}</td>
-                                    <td>{{$bikeSale->grade}}</td>
-                                    <td>{{$bikeSale->price}}</td>
-
-                                    <td>{{$bikeSale->bike_sale_pivot->quantity_sold}}</td>
-                                    <td>{{$sale->created_at}}</td>
-                                    <td>{{$sale->profit}}</td>
-                                </tr>
-                                @endforeach
-                            @endforeach
-                        </tbody>
-                    </table>
-                    <table class="table table-bordered">
-                        <tbody>
-                            <tr>
-                                <th scope="col">Total Profit (CAD)</th>
-
-                                <td>{{$totalSalesProfit}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <!--End of monthly sales tab-->             
-
-                <!--Yearly sales tab-->             
-                <div class="tab-pane fade" id="yearly_sales">
-                    <table class="table table-borderless">
-                        <tbody>
-                            <tr>
-                                <td align='left'><h3>This year's sales</h3></td> <!--Make the year dynamic--><!--Inline css (align='left'), for better practice, can be taken out into a css file-->
-                                <td align='right'>
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Choose a year</button>
-                                        <div class="dropdown-menu">
-                                            <!--Dummy values, make them dynamic in the future-->
-                                            <a class="dropdown-item" href="#">2020</a>
-                                            <a class="dropdown-item" href="#">2019</a>
-                                            <a class="dropdown-item" href="#">2018</a>
-                                        </div>
-                                    </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th scope="col" rowspan = "2">Sales ID</th>
-                                <th scope="col" colspan = "7">Bicycle Specifications</th>
-                                <th scope="col" rowspan = "2">Quantity Sold</th>
-                                <th scope="col" rowspan = "2">Date Sold</th>
-                                <th scope="col" rowspan = "2">Profit (CAD)</th>
-                            </tr>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Type</th>
-                                <th scope="col">Size</th>
-                                <th scope="col">Color</th>
-                                <th scope="col">Finish</th>
-                                <th scope="col">Grade</th>
-                                <th scope="col">Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!--Dummy values, make them dynamic in the future-->
-                            @foreach ($sales as $sale)
-                                @foreach ($sale->bikes as $bikeSale)
-                                <tr>
-                                    <td>{{$sale->id}}</td>
-                                    
-                                    <td>{{$bikeSale->bike_sale_pivot->bike_id}}</td> <!--We need to use bike_sale_pivot to get the bike_id in the bike_sale table-->
-                                    <td>{{$bikeSale->type}}</td>
-                                    <td>{{$bikeSale->size}}</td>
-                                    <td>{{$bikeSale->color}}</td>
-                                    <td>{{$bikeSale->finish}}</td>
-                                    <td>{{$bikeSale->grade}}</td>
-                                    <td>{{$bikeSale->price}}</td>
-
-                                    <td>{{$bikeSale->bike_sale_pivot->quantity_sold}}</td>
-                                    <td>{{$sale->created_at}}</td>
-                                    <td>{{$sale->profit}}</td>
-                                </tr>
-                                @endforeach
-                            @endforeach
-                        </tbody>
-                    </table>
-                    <table class="table table-bordered">
-                        <tbody>
-                            <tr>
-                                <th scope="col">Total Profit (CAD)</th>
-
-                                <td>{{$totalSalesProfit}}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <!--End of yearly sales tab-->             
             </div>
             <!--End of card body-->             
         </div>
